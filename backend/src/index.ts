9974f1962a055139ae9006129cca091467efe88f
Anurag Maurya <anuragmaurya566@gmail.com>
2025-01-24 14:29:35 +0530

    intern

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,11 +39,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("API is Working");
 });
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
-
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+  app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+  })
+}
 // Connect to the database and start the server
 connectDB()
   .then(() => {
